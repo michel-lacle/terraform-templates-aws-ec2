92bb96a88bb0b7c9fe5958ffa7be8d11046b33db
papiamento <1024836+papiamento@users.noreply.github.com>
2020-03-20 13:01:02 -0400

    create ec2 isntance

---
+++ aws_ec2.tf
@@ -0,0 +1,20 @@
+
+resource "aws_instance" "example" {
+  count	        = 1
+  ami           = "ami-0a887e401f7654935"
+  instance_type = "t2.micro"
+  
+  key_name = "ExampKP"
+
+  vpc_security_group_ids = ["sg-0d9a3a138f6746c16"]
+
+ tags = {
+    owner = "terraform-templates-aws-ec2"
+  }
+}
+
+# output "aws_instance_ip" {
+
+#   value = aws_instance.example[0].public_ip
+#   description = "The public IP address of our instance"
+# }
