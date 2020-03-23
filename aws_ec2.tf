b12d6e5387549f940834ebe06c362c9d2ef6f401
papiamento <1024836+papiamento@users.noreply.github.com>
2020-03-23 10:54:08 -0400

    fix ingress port range

--- aws_ec2.tf
+++ aws_ec2.tf
@@ -14,7 +14,7 @@ resource "aws_security_group" "template-ec2-basic" {
   ingress {
     description = "TLS from VPC"
     from_port   = 80
-    to_port     = 22
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0", "::/0"]
   }
